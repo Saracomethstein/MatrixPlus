4697269bb1d56b3715b4842484b57c05df822b68
Baratheon Alaina <barathea@va-b5.kzn.21-school.ru>
2024-05-12 14:37:57 +0300

    fix code style

--- CheckCorrect/CheckCorrect.cpp
+++ CheckCorrect/CheckCorrect.cpp
@@ -6,6 +6,4 @@ bool Matrix::IsEqualSizeMatrix(const Matrix& other) const {
   return rows_ == other.rows_ && cols_ == other.cols_ ? true : false;
 }
 
-bool Matrix::IsCorrect() const {
-  return rows_ < 0 || cols_ < 0 ? false : true;
-}
+bool Matrix::IsCorrect() const { return rows_ < 0 || cols_ < 0 ? false : true; }
